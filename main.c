f6727b9c64326508d14fccf6bc66883ac9de7112
katjan <katjaro@kth.se>
2015-04-22 18:34:17 +0200

    exit ish

--- main.c
+++ main.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, char *argv[], char *envp[])
-{
-    char c = '\0';
-    printf("\n[MY_SHELL ] ");
-    while(c != EOF) {
-        c = getchar();
-        if(c == '\n')
-            printf("[MY_SHELL ] ");
+#ifdef _WIN32
+#include <windows.h>
+#define chdir _chdir
+
+#else
+#include <unistd.h>
+#endif
+
+#define MAX_LENGTH 80
+#define DELIMS " \t\r\n"
+
+int main(int argc, char *argv[]) {
+    char *cmd;
+    char line[MAX_LENGTH];
+
+    while (1) {
+        printf("fake_shell$ ");
+        if (!fgets(line, MAX_LENGTH, stdin)) break;
+
+        // Parse and execute command
+        if ((cmd = strtok(line, DELIMS))) {
+            // Clear errors
+            errno = 0;
+
+            if (strcmp(cmd, "cd") == 0) {
+                char *arg = strtok(0, DELIMS);
+
+                if (!arg) fprintf(stderr, "cd missing argument.\n");
+                else chdir(arg);
+
+            } else if (strcmp(cmd, "exit") == 0) {
+                //here we should terminates all remaining processes
+                // started from the shell in an orderly manner before exiting the shell itself
+                break;
+
+            } else system(line);
+
+            if (errno) perror("Command failed");
+        }
     }
-    printf("\n");
+
     return 0;
 }
